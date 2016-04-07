e1158aa1505069d09d678c75b972f46cc0355808
Stanislava Kaukova <sisiivanovva@gmail.com>
2016-04-07 10:37:42 +0300

    make SingleObject final

--- src/com/clouway/singleton/SingleObject.java
+++ src/com/clouway/singleton/SingleObject.java
@@ -4,7 +4,7 @@ package com.clouway.singleton;
  * @author Stanislava Kaukova(sisiivanovva@gmail.com)
  */
 public class SingleObject {
-    private static SingleObject ourInstance = new SingleObject();
+    private static final SingleObject ourInstance = new SingleObject();
 
     private SingleObject() {
     }
