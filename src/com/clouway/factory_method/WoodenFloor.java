7cc7cd0eee856fdf6b3b950dae4dc08794684d39
Stanislava Kaukova <sisiivanovva@gmail.com>
2016-04-07 08:41:18 +0300

    Refactor

--- src/com/clouway/factory_method/WoodenFloor.java
+++ src/com/clouway/factory_method/WoodenFloor.java
@@ -4,8 +4,21 @@ package com.clouway.factory_method;
  * @author Stanislava Kaukova(sisiivanovva@gmail.com)
  */
 public class WoodenFloor implements Floor {
+    private String color;
+
+    public WoodenFloor(String color) {
+        this.color = color;
+    }
+
     @Override
     public String getColor() {
-        return "This floor is wooden";
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "WoodenFloor{" +
+                "color='" + color + '\'' +
+                '}';
     }
 }
