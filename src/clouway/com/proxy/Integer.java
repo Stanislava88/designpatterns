73c4fceb09ad50ff196be2119006d0e64bb54dff
Stanislava Kaukova <sisiivanovva@gmail.com>
2016-04-11 17:37:46 +0300

    Refactor

--- src/clouway/com/proxy/Integer.java
+++ src/clouway/com/proxy/Integer.java
@@ -1,8 +1,13 @@
 package clouway.com.proxy;
 
 /**
+ * The implementation of this interface will be used to display Integer
+ *
  * @author Stanislava Kaukova(sisiivanovva@gmail.com)
  */
 interface Integer {
+    /**
+     * display number
+     */
     void display();
 }
